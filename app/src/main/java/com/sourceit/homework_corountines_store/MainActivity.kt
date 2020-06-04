387ef5ab986cf1aad1605a4db01734402e7e572f
kuzavkov <serg1992kuzavkov@gmail.com>
2020-06-04 14:44:54 +0300

    fix wrong calc

--- app/src/main/java/com/sourceit/homework_corountines_store/MainActivity.kt
+++ app/src/main/java/com/sourceit/homework_corountines_store/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val numOfGoodsInStoreOnStart = AtomicInteger(15000)
+        var numOfGoodsInStoreOnStart = AtomicInteger(15000)
         val maxNumOfGoods = numOfGoodsInStoreOnStart.toInt() * 4
         var balanceOfGoodsInTime = AtomicInteger(0)
         val stringBuffer = StringBuffer()
@@ -21,11 +21,15 @@ class MainActivity : AppCompatActivity() {
         txt_available_goods.text = "$numOfGoodsInStoreOnStart"
         GlobalScope.launch {
             while (balanceOfGoodsInTime.toInt() in 0..maxNumOfGoods) {
-                numOfGods = (1000..3000).random()
+
                 time = (10000..15000L).random()
                 delay(time)
-                balanceOfGoodsInTime =
+                numOfGods = (3000..6000).random()
+                balanceOfGoodsInTime = if (balanceOfGoodsInTime.toInt() == 0) {
                     AtomicInteger(numOfGoodsInStoreOnStart.toInt() + numOfGods)
+                } else {
+                    AtomicInteger(balanceOfGoodsInTime.toInt() + numOfGods)
+                }
                 stringBuffer.append(", поставка: $numOfGods за ${time / 1000} сек")
                 withContext(Dispatchers.Main) {
                     txt_logbook.text = "$stringBuffer"
@@ -37,9 +41,12 @@ class MainActivity : AppCompatActivity() {
             while (balanceOfGoodsInTime.toInt() in 0..maxNumOfGoods) {
                 time = (1000..5000L).random()
                 delay((2000..5000L).random())
-                numOfGods = (2000..5000).random()
-                balanceOfGoodsInTime =
+                numOfGods = (1..5).random()
+                balanceOfGoodsInTime = if (balanceOfGoodsInTime.toInt() == 0) {
                     AtomicInteger(numOfGoodsInStoreOnStart.toInt() - numOfGods)
+                } else {
+                    AtomicInteger(balanceOfGoodsInTime.toInt() - numOfGods)
+                }
                 stringBuffer.append(", приобрели: $numOfGods за ${time / 1000} сек ")
                 withContext(Dispatchers.Main) {
                     txt_logbook.text = "$stringBuffer"
